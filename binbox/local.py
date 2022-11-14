39f8b5790db0d836017317a147d2b92b4b4d8fa4
muziwenwu <li296641798@gmail.com>
2022-11-14 19:47:59 +0800

    [update] local 接口添加 log 参数，以便传入文件路径，会将 log 实时写入该文件

--- binbox/local.py
+++ binbox/local.py
@@ -5,22 +5,31 @@ except:
     import _tool
 
 
-def local(command:str):
+def local(command:str, log=None):
     """ 在本地执行命令，并把结果(stdout/stderr)打印
         返回 stdout 
     """
+    if log !=None:
+        fp = open(log, "w")
+
     command=_tool.command_clear(command)
+    out_list = []
     print(f"[binbox] local:{command}")
     ret = subprocess.Popen(command, shell=True,
                      stdin=subprocess.PIPE,
                      stdout=subprocess.PIPE,
-                     stderr=subprocess.PIPE, text=True, bufsize=1)
+                     stderr=subprocess.PIPE, universal_newlines=True, bufsize=1)
     for line in ret.stdout:
         print(line, end="")
+        out_list.append(line.strip())
+        if log !=None:
+            fp.write(line)
+            fp.flush()
     ret.wait()
-
+    if log !=None:
+        fp.close()
     if ret.returncode == 0:
-        return ret.stdout
+        return out_list
     else:
         for line in ret.stderr:
             print(line, end="")
